b4391cd4ad2f5e91564083db2ad959ca2ddc6e94
matvis01 <mati.siwy@op.pl>
2023-09-20 21:08:16 +0200

    first

--- src/game-key-marketplace.ts
+++ src/game-key-marketplace.ts
@@ -1,51 +1,77 @@
 import {
   ItemBought as ItemBoughtEvent,
   ItemCancelled as ItemCancelledEvent,
-  ItemListed as ItemListedEvent
-} from "../generated/GameKeyMarketplace/GameKeyMarketplace"
-import { ItemBought, ItemCancelled, ItemListed } from "../generated/schema"
+  ItemListed as ItemListedEvent,
+} from "../generated/GameKeyMarketplace/GameKeyMarketplace";
+import { ItemBought, ItemCancelled, ItemListed } from "../generated/schema";
+import { BigInt, Address, Bytes } from "@graphprotocol/graph-ts";
 
 export function handleItemBought(event: ItemBoughtEvent): void {
-  let entity = new ItemBought(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.gameId = event.params.gameId
-  entity.price = event.params.price
-  entity.buyer = event.params.buyer
+  let id = getIdFromEventParams(
+    event.params.gameId,
+    event.params.price,
+    event.params.buyer
+  );
+  let itemBought = ItemBought.load(id);
+  let itemListed = ItemListed.load(id);
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  if (!itemBought) {
+    itemBought = new ItemBought(id);
+    itemBought.gameId = event.params.gameId;
+    itemBought.price = event.params.price;
+    itemBought.buyer = event.params.buyer;
+    itemBought.numOfItems = BigInt.fromI32(1);
+  } else {
+    itemBought.numOfItems = itemBought.numOfItems.plus(BigInt.fromI32(1));
+  }
+  itemBought.save();
 
-  entity.save()
+  if (itemListed) {
+    itemListed.numOfItems = itemListed.numOfItems.minus(BigInt.fromI32(1));
+    itemListed.save();
+  }
 }
 
 export function handleItemCancelled(event: ItemCancelledEvent): void {
-  let entity = new ItemCancelled(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.gameId = event.params.gameId
-  entity.price = event.params.price
-  entity.seller = event.params.seller
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let id = getIdFromEventParams(
+    event.params.gameId,
+    event.params.price,
+    event.params.seller
+  );
+  let itemCancelled = ItemListed.load(id);
+  if (itemCancelled) {
+    itemCancelled.numOfItems = itemCancelled.numOfItems.minus(
+      BigInt.fromI32(1)
+    );
+    itemCancelled.save();
+  }
 }
 
 export function handleItemListed(event: ItemListedEvent): void {
-  let entity = new ItemListed(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.gameId = event.params.gameId
-  entity.price = event.params.price
-  entity.seller = event.params.seller
+  let id = getIdFromEventParams(
+    event.params.gameId,
+    event.params.price,
+    event.params.seller
+  );
+  let item = ItemListed.load(id);
+  if (!item) {
+    item = new ItemListed(id);
+    item.gameId = event.params.gameId;
+    item.price = event.params.price;
+    item.seller = event.params.seller;
+    item.numOfItems = BigInt.fromI32(1);
+  } else {
+    item.numOfItems = item.numOfItems.plus(BigInt.fromI32(1));
+  }
+  item.save();
+}
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+function getIdFromEventParams(
+  gameId: BigInt,
+  price: BigInt,
+  seller: Address
+): string {
+  return (
+    gameId.toString() + "-" + price.toString() + "-" + seller.toHexString()
+  );
 }
